4cbace19f8f488f81b9827018bace8f1300366be
CodeCrafter45 <maheshkumbhar720@gmail.com>
2026-08-03 21:25:39 +0530

    Project setup and Url class designed

---
+++ src/main/java/com/urlShortner/repository/UrlRepository.java
@@ -0,0 +1,8 @@
+package com.urlShortner.repository;
+
+import com.urlShortner.entity.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UrlRepository extends JpaRepository<Url,Long> {
+
+}
